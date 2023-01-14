1da0545c67b480a98151c31162b336474aaf219e
Shay shimoni <shay.shimoni@msapps.mobi>
2023-01-14 23:42:33 +0200

    Add state flows and subscription logic.

--- app/src/main/java/com/shay/currencypairs/ui/CurrencyPairsViewModel.kt
+++ app/src/main/java/com/shay/currencypairs/ui/CurrencyPairsViewModel.kt
@@ -1,6 +1,44 @@
 package com.shay.currencypairs.ui
 
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.shay.currencypairs.WebSocketService
+import com.shay.currencypairs.data.entities.CurrencyPairType
+import com.shay.currencypairs.data.responses.CurrencyPairsSubscriptionResponse
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class CurrencyPairsViewModel: ViewModel() {
+class CurrencyPairsViewModel : ViewModel() {
+
+    private val _currencyPair: MutableStateFlow<List<*>> = MutableStateFlow(emptyList<List<*>>())
+    val currencyPair: StateFlow<List<*>> by lazy { _currencyPair }
+
+    private var count = 0
+    private val channels = mutableListOf<CurrencyPairsSubscriptionResponse>()
+
+    private val service: WebSocketService =
+        WebSocketService(object : WebSocketService.CurrencyPairCallBack {
+            override fun onCurrencyPairUpdate(response: String) {
+
+                if (count == 0) {
+                    count++
+                } else if (count <= CurrencyPairType.values().size) {
+                    channels.add(Gson().fromJson(response, CurrencyPairsSubscriptionResponse::class.java))
+                    count++
+                } else {
+                    if (!response.contains("heartbeat")) {
+                        _currencyPair.update {
+                            Gson().fromJson(response, List::class.java)
+                        }
+                    }
+                }
+
+            }
+        })
+
+    fun subscribe() {
+        service.subscribeToCurrencyPair()
+    }
+
 }
